d4d74a8a0aadc6874dd078a6dc69c95001a86f0b
Abdelrahman74S <bdalrhmnr927@gmail.com>
2026-01-15 09:18:04 +0200

    Version 0.4 - Hotel - view

---
+++ Hotel/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from .models import Hotel, HotelImage, RoomFeature, RoomCategory, Room
+
+class TailwindFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white'
+            })
+
+class HotelForm(TailwindFormMixin, forms.ModelForm):
+    class Meta:
+        model = Hotel
+        fields = ['name', 'slug', 'address', 'description', 'website']
+        widgets = {
+            'address': forms.Textarea(attrs={'rows': 2, 'placeholder': 'Enter full hotel address...'}),
+            'description': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Describe your hotel...'}),
+            'slug': forms.TextInput(attrs={'placeholder': 'hotel-name-example'}),
+        }
+
+class HotelImageForm(TailwindFormMixin, forms.ModelForm):
+    class Meta:
+        model = HotelImage
+        fields = ['hotel', 'image', 'is_primary']
+        help_texts = {
+            'is_primary': 'Check this if it is the main cover photo.'
+        }
+
+class RoomFeatureForm(TailwindFormMixin, forms.ModelForm):
+    class Meta:
+        model = RoomFeature
+        fields = ['name', 'icon']
+
+class RoomCategoryForm(TailwindFormMixin, forms.ModelForm):
+    class Meta:
+        model = RoomCategory
+        fields = ['hotel', 'title', 'base_price', 'capacity_adults', 'capacity_child', 'description', 'features']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+            'features': forms.CheckboxSelectMultiple(),
+        }
+
+class RoomForm(TailwindFormMixin, forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = ['category', 'room_number', 'room_type', 'price_override', 'is_available']
+        help_texts = {
+            'price_override': 'Optional: Set a specific price for this room only.'
+        }
--- Hotel/views.py
+++ Hotel/views.py
@@ -1,3 +1,213 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Hotel, HotelImage, RoomFeature, RoomCategory, Room
+from .forms import HotelForm, HotelImageForm, RoomFeatureForm, RoomCategoryForm, RoomForm
+from django.views.generic import CreateView, DetailView, UpdateView, ListView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from .Permissions import ManagerRequiredMixin
 
-# Create your views here.
+# View for Hotel
+class HotelCreateView(LoginRequiredMixin,ManagerRequiredMixin, CreateView):
+    model = Hotel
+    form_class = HotelForm
+    template_name = 'hotel/hotel_form.html'
+    success_url = reverse_lazy('hotel_list')
+    
+class HotelListView(ListView):
+    model = Hotel
+    template_name = 'hotel/hotel_list.html'
+    context_object_name = 'hotels'
+    
+    def get_queryset(self):
+        return Hotel.objects.all().prefetch_related('images')
+
+class HotelDetailView(DetailView):
+    model = Hotel
+    template_name = 'hotel/hotel_detail.html'
+    context_object_name = 'hotel'
+    slug_url_kwarg = 'slug' 
+    
+    def get_queryset(self):
+        return Hotel.objects.all().prefetch_related(
+            'images', 
+            'categories__features', 
+            'categories__rooms'
+        )
+    
+class HotelUpdateView(LoginRequiredMixin,ManagerRequiredMixin, UpdateView):
+    model = Hotel
+    form_class = HotelForm
+    template_name = 'hotel/hotel_form.html'
+    success_url = reverse_lazy('hotel_list')
+    
+@login_required
+def hotel_delete_view(request, pk):
+    if not request.user.is_authenticated or request.user.user_type != 'M':
+        return redirect('login')
+        
+    hotel = get_object_or_404(Hotel, pk=pk)
+    if request.method == 'POST':
+        hotel.delete()
+        return redirect('hotel_list')
+    return render(request, 'hotel/hotel_confirm_delete.html', {'hotel': hotel})
+
+
+# View for RoomCategory 
+class RoomCategoryCreateView(LoginRequiredMixin,ManagerRequiredMixin, CreateView):
+    model = RoomCategory
+    form_class = RoomCategoryForm
+    template_name = 'hotel/roomcategory_form.html'
+    success_url = reverse_lazy('hotel_list')
+    
+class RoomCategoryListView(ListView):
+    model = RoomCategory
+    template_name = 'hotel/roomcategory_list.html'
+    context_object_name = 'room_categories'
+    
+    def get_queryset(self):
+        return RoomCategory.objects.all.select_related('hotel').prefetch_related('features', 'rooms')
+    
+class RoomCategoryDetailView(DetailView):
+    model = RoomCategory
+    template_name = 'hotel/roomcategory_detail.html'
+    context_object_name = 'room_category'
+    
+    def get_queryset(self):
+        return RoomCategory.objects.all().select_related('hotel').prefetch_related('features', 'rooms')
+    
+class RoomCategoryUpdateView(LoginRequiredMixin,ManagerRequiredMixin, UpdateView):
+    model = RoomCategory
+    form_class = RoomCategoryForm
+    template_name = 'hotel/roomcategory_form.html'
+    success_url = reverse_lazy('hotel_list')
+    
+@login_required
+def roomcategory_delete_view(request, pk):
+    if not request.user.is_authenticated or request.user.user_type != 'M':
+        return redirect('login')
+        
+    room_category = get_object_or_404(RoomCategory, pk=pk)
+    if request.method == 'POST':
+        room_category.delete()
+        return redirect('hotel_list')
+    return render(request, 'hotel/roomcategory_confirm_delete.html', {'room_category': room_category})
+
+# views for HotelImage , and Room can be implemented similarly.
+class HotelImageCreateView(LoginRequiredMixin,ManagerRequiredMixin, CreateView):
+    model = HotelImage
+    form_class = HotelImageForm
+    template_name = 'hotel/hotel_image_form.html'
+    success_url = reverse_lazy('hotel_list')
+
+class HotelImageListView(ListView):
+    model = HotelImage
+    template_name = 'hotel/hotel_image_list.html'
+    context_object_name = 'hotel_images'
+
+    def get_queryset(self):
+        return HotelImage.objects.all().select_related('hotel')
+    
+class HotelImageDetailView(DetailView):
+    model = HotelImage
+    template_name = 'hotel/hotel_image_detail.html'
+    context_object_name = 'hotel_image'
+    
+    def get_queryset(self):
+        return HotelImage.objects.all().select_related('hotel')
+
+class HotelImageUpdateView(LoginRequiredMixin,ManagerRequiredMixin, UpdateView):
+    model = HotelImage
+    form_class = HotelImageForm
+    template_name = 'hotel/hotel_image_form.html'
+    success_url = reverse_lazy('hotel_list')
+    
+@login_required
+def hotel_image_delete_view(request, pk):
+    if not request.user.is_authenticated or request.user.user_type != 'M':
+        return redirect('login')
+        
+    hotel_image = get_object_or_404(HotelImage, pk=pk)
+    if request.method == 'POST':
+        hotel_image.delete()
+        return redirect('hotel_list')
+    return render(request, 'hotel/hotel_image_confirm_delete.html', {'hotel_image': hotel_image})
+
+# views for RoomFeature
+class RoomFeatureCreateView(LoginRequiredMixin,ManagerRequiredMixin, CreateView):
+    model = RoomFeature
+    form_class = RoomFeatureForm
+    template_name = 'hotel/room_feature_form.html'
+    success_url = reverse_lazy('hotel_list')
+
+class RoomFeatureListView(ListView):
+    model = RoomFeature
+    template_name = 'hotel/room_feature_list.html'
+    context_object_name = 'room_features'
+
+    def get_queryset(self):
+        return RoomFeature.objects.all()   
+    
+class RoomFeatureDetailView(DetailView):
+    model = RoomFeature
+    template_name = 'hotel/room_feature_detail.html'
+    context_object_name = 'room_feature'
+    
+    def get_queryset(self):
+        return RoomFeature.objects.all()
+
+class RoomFeatureUpdateView(LoginRequiredMixin,ManagerRequiredMixin, UpdateView):
+    model = RoomFeature
+    form_class = RoomFeatureForm
+    template_name = 'hotel/room_feature_form.html'
+    success_url = reverse_lazy('hotel_list')
+
+@login_required
+def room_feature_delete_view(request, pk):
+    if not request.user.is_authenticated or request.user.user_type != 'M':
+        return redirect('login')  
+    room_feature = get_object_or_404(RoomFeature, pk=pk)
+    if request.method == 'POST':
+        room_feature.delete()
+        return redirect('hotel_list')
+    return render(request, 'hotel/room_feature_confirm_delete.html', {'room_feature': room_feature})
+
+# views for Room
+class RoomCreateView(LoginRequiredMixin,ManagerRequiredMixin, CreateView):
+    model = Room
+    form_class = RoomForm
+    template_name = 'hotel/room_form.html'
+    success_url = reverse_lazy('hotel_list')
+    
+class RoomListView(ListView):
+    model = Room
+    template_name = 'hotel/room_list.html'
+    context_object_name = 'rooms'
+    
+    def get_queryset(self):
+        return Room.objects.all().select_related('category')
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = 'hotel/room_detail.html'
+    context_object_name = 'room'
+    
+    def get_queryset(self):
+        return Room.objects.all().select_related('category')    
+    
+class RoomUpdateView(LoginRequiredMixin,ManagerRequiredMixin, UpdateView):
+    model = Room
+    form_class = RoomForm
+    template_name = 'hotel/room_form.html'
+    success_url = reverse_lazy('hotel_list')
+    
+@login_required
+def room_delete_view(request, pk):  
+    if not request.user.is_authenticated or request.user.user_type != 'M':
+        return redirect('login')
+        
+    room = get_object_or_404(Room, pk=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('hotel_list')
+    return render(request, 'hotel/room_confirm_delete.html', {'room': room})
